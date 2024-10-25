c654fd677499a1373c2f2bf4f16d32e0a5cf7414
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-10-25 03:04:01 -0300

    Se finaliza modificar usuario
    Se agrega código JS para guardar cambios en un usuario específico
    
    Se agrega documentación de las funciones JavaScript
    
    Se agrega lógica para eliminar usuario
    
    Se agrega lógica para recibir mensajes de success y error en el layout

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import User
+from django.contrib import messages
+import json
 
 # Create your views here.
 def index(request):
@@ -43,38 +45,51 @@ def register(request):
 
 
 def users_list(request):
-
     if request.method == 'POST': 
-        
-        new_user_id = request.POST.get('userId')
-        new_user_name = request.POST.get('name')
-        new_user_last_name = request.POST.get('last_name')
-        new_user_speciality = request.POST.get('speciality')
-        new_user_email = request.POST.get('email')
-        new_user_address = request.POST.get('address')
-        new_user_phone = request.POST.get('phone')
+        try:
+            eliminar = request.POST.get('deleteUser')
+            if eliminar:
+                user = User.objects.get(id=eliminar)
+                user.delete()
+                print("Usuario eliminado")
+                messages.success(request, 'Usuario eliminado exitosamente!')
+                
+            else:
+            
+                new_user_id = request.POST.get('userId')
+                new_user_name = request.POST.get('name')
+                new_user_last_name = request.POST.get('last_name')
+                new_user_speciality = request.POST.get('speciality')
+                new_user_email = request.POST.get('email')
+                new_user_address = request.POST.get('address')
+                new_user_phone = request.POST.get('phone')
 
-        print("Info obtenida:")
-        print(new_user_id)
-        print(new_user_name)
-        print(new_user_last_name)
-        print(new_user_speciality)
-        print(new_user_email)
-        print(new_user_address)
-        print(new_user_phone)
+                print("Info obtenida:")
+                print(new_user_id)
+                print(new_user_name)
+                print(new_user_last_name)
+                print(new_user_speciality)
+                print(new_user_email)
+                print(new_user_address)
+                print(new_user_phone)
+                
+                user = User.objects.get(id=new_user_id)
+                user.name = new_user_name
+                user.last_name = new_user_last_name
+                user.speciality = new_user_speciality
+                user.email = new_user_email
+                user.address = new_user_address
+                user.phone = new_user_phone
+                user.save()
+                messages.success(request, 'Usuario modificado exitosamente!')
+        except Exception as e:
+            print(e)
+            messages.error(request, 'Hubo un error!')
         
-        user = User.objects.get(id=new_user_id)
-        user.name = new_user_name
-        user.last_name = new_user_last_name
-        user.speciality = new_user_speciality
-        user.email = new_user_email
-        user.address = new_user_address
-        user.phone = new_user_phone
-        user.save()
-        
-    
     users = User.objects.all()
-    return render(request, 'users_list.html', {'users': users})
+    users_to_json = json.dumps(list(users.values()))
+    print(users_to_json)
+    return render(request, 'users_list.html', {'users': users, 'users_to_json': users_to_json})
 
 def update(request):
     return render(request, 'update.html')
